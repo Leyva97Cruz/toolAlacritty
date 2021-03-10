6b8f4b52de34963b372ce689a153a196390f8918
Aaron Leyva Cruz <aaronleyvacruz@gmail.com>
2021-03-09 18:29:32 -0600

    Ya funciona git addgit add

--- src/pycolor.py
+++ src/pycolor.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-import yaml
+import ruamel.yaml
 import sys
 
 themes = "themes/nord.yaml"
 FileAlacritty = "example.yml"
+yaml = ruamel.yaml.YAML()
 
 
 if __name__ == '__main__':
     updateChange = open(FileAlacritty,'r')
     addLines = open(themes,'r')
 
-    readAlacritty = yaml.load(updateChange,Loader=yaml.FullLoader)
-    readThemes = yaml.load(addLines,Loader=yaml.FullLoader)
-    
+    readAlacritty = yaml.load(updateChange)
+    readThemes = yaml.load(addLines)
+
     readAlacritty['colors'] = readThemes['colors']
     updateChange.close()
     addLines.close()
 
     updateFile = open(FileAlacritty,'w')
-    resulChanges = yaml.dump_all(readAlacritty,updateFile,default_flow_style=False,sort_keys=False)
+    yaml.dump(readAlacritty,updateFile)
     updateFile.close()
-
